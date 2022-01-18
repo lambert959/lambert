7e731cd7dee435d661b33af8d190eeccf8e35d73
lambert959 <735567101@qq.com>
2022-01-18 16:02:11 +0800

    Delete egg-vue-ssr directory

--- egg-vue-ssr/app/router.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {Egg.Application} app - egg application
- */
-module.exports = app => {
-  require('./routers/api')(app)
-  require('./routers/resources')(app)
-}
--- egg-vue-ssr/app/web/layout/default.js
+++
@@ -1,4 +0,0 @@
-import '../asset/css/index.scss'
-import createLayout from './layout'
-const tpl = `<div id="app" data-server-rendered="true"><slot></slot></div>`
-export default createLayout('Layout', {}, tpl)
